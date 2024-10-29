fe55b23cf39b01a26cd3747739604ab7190aefdc
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2024-10-30 00:00:40 +0100

    feat: added react-router-dom dependency and implemented routing in App.jsx, removed individual component imports and replaced with route configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/navbar/navbar.components';
-import HeroSection from './components/hero/hero.components';
-import FeatureSection from './components/feature/feature.component';
-import AboutUsAndMission from './components/aboutAndMission/aboutUs.component';
-import CategorySection from './components/categorySection/categories.component';
-import WhyWeStarted from './components/whyWeStarted/whyWeStarted.components';
-import TopAuthors from './components/topAuthors/topAuthors.components';
-import FeaturedIn from './components/clout/clout.components';
-import Testimonials from './components/testimonials/testimonials.components';
-import JoinUs from './components/joinus/joinus.component';
 import Footer from './components/footer/footer.component';
+
+import Home from './routes/home/home.routes';
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <HeroSection />
-      <FeatureSection />
-      <AboutUsAndMission />
-      <CategorySection />
-      <WhyWeStarted />
-      <TopAuthors />
-      <FeaturedIn />
-      <Testimonials />
-      <JoinUs />
+        <Routes>
+          <Route path='/' index element={<Home />} />
+        </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+    // <>
+    //   <Navbar />
+    //   <HeroSection />
+    //   <FeatureSection />
+    //   <AboutUsAndMission />
+    //   <CategorySection />
+    //   <WhyWeStarted />
+    //   <TopAuthors />
+    //   <FeaturedIn />
+    //   <Testimonials />
+    //   <JoinUs />
+    //   <Footer />
+    // </>
