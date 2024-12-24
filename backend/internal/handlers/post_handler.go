6703946c14aa0e1ee9413a164592c8592d16d358
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2024-12-24 22:41:59 +0100

    fix: refactored post handler

--- backend/internal/handlers/post_handler.go
+++ backend/internal/handlers/post_handler.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/Onyekachukwu-Nweke/piko-blog/backend/internal/interfaces"
+	"github.com/Onyekachukwu-Nweke/piko-blog/backend/internal/models"
+	"github.com/go-playground/validator/v10"
+	"log"
 	"net/http"
 )
 
@@ -13,6 +17,36 @@ func NewPostHandler(service interfaces.PostStore) *PostHandler {
 	return &PostHandler{Service: service}
 }
 
-func (handler *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
+func (h *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
+	var post models.Post
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
 
+	validate := validator.New()
+	err := validate.Struct(post)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		//json.NewEncoder(w).Encode(Response{Message: "Post not valid"})
+		// TODO: Add Logger
+		log.Print(err)
+		return
+	}
+
+	createdPost, err := h.Service.CreatePost(r.Context(), post)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		//json.NewEncoder(w).Encode(Response{Message: "Post could not created"})
+		// TODO: Add Logger
+		log.Print(err)
+		return
+	}
+
+	// TODO: Add Logger
+	log.Print("Post Successfully Created")
+
+	if err := json.NewEncoder(w).Encode(createdPost); err != nil {
+		panic(err)
+	}
 }
