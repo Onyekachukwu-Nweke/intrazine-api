2047a19a76843ff7e0c01b391e62fe89d63f5746
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2024-12-24 22:57:13 +0100

    fix: added json middleware and routes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"github.com/Onyekachukwu-Nweke/piko-blog/backend/internal/handlers"
+	"github.com/Onyekachukwu-Nweke/piko-blog/backend/pkg/middleware"
+	"github.com/gorilla/mux"
+)
+
+// RegisterRoutes sets up all routes for the application.
+func RegisterRoutes(router *mux.Router, postHandler *handlers.PostHandler) {
+	// Apply JSON middleware globally
+	router.Use(middleware.JSONMiddleware)
+	// Post routes
+	router.HandleFunc("/posts", postHandler.CreatePost).Methods("POST") // Create a new post
+	//router.HandleFunc("/posts", postHandler.GetAllPosts).Methods("GET") // Get all posts
+}
