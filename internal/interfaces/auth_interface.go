3347ca0ad87ac5e10ce45aa3e9052dc969260f28
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2025-01-21 22:16:30 +0100

    chore: remove deprecated backend and frontend files
    - Deleted unused backend files including Dockerfile, Go modules, various service, repository, and model files to streamline the project structure.
    - Removed frontend components, assets, and configuration files that are no longer in use, including the main application files and styles.
    - Cleaned up migration scripts and other related files to ensure a more maintainable codebase.

---
+++ internal/interfaces/auth_interface.go
@@ -0,0 +1,14 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/Onyekachukwu-Nweke/piko-blog/backend/internal/models"
+)
+
+type AuthService interface {
+	Signup(ctx context.Context, user models.User) (models.User, error)
+	Login(ctx context.Context, username string, password string) (models.User, error)
+	CheckUserExists(ctx context.Context, username, email string) (exists bool, field string, err error)
+	ForgotPassword(ctx context.Context, username string) (string, error)
+}
